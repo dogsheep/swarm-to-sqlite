d3c4ab2848ea606417150f377a82e66ca7887c54
Andreas Madsack <andreas@madflex.de>
2020-03-28 03:28:10 +0100

    Don't break if source is missing (#6)
    This broke for very old checkins from 2010 with no source set. Thanks, @mfa!

--- swarm_to_sqlite/utils.py
+++ swarm_to_sqlite/utils.py
@@ -49,7 +49,7 @@ def save_checkin(checkin, db):
     checkin["created"] = datetime.datetime.utcfromtimestamp(
         checkin["createdAt"]
     ).isoformat()
-    checkin["source"] = db["sources"].lookup(checkin["source"])
+    checkin["source"] = db["sources"].lookup(checkin["source"]) if "source" in checkin else None
     users_with = checkin.pop("with", None) or []
     users_likes = []
     for group in checkin["likes"]["groups"]:
